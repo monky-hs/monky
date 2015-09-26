66183cf25d15201227426d1f8bb5f41f878ac5ae
Markus Ongyerth <ongy@ongy.net>
2015-09-26 22:09:52 +0200

    Add LGPL notices

--- Monky/Battery.hs
+++ Monky/Battery.hs
@@ -1,3 +1,21 @@
+{-
+    Copyright 2015 Markus Ongyerth, Stephan Guenther
+
+    This file is part of Monky.
+
+    Monky is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Monky is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Monky.  If not, see <http://www.gnu.org/licenses/>.
+-}
 {-|
 Module      : Monky.Battery
 Description : Allows access to information about a battery connected to the system
