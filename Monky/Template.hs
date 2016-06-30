b0a49c3ea1778cad0292aedb3b70ac136892919e
Markus Ongyerth <ongy@ongy.net>
2016-06-30 13:20:18 +0200

    Make monky ghc-8 ready

--- Monky/Template.hs
+++ Monky/Template.hs
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with Monky.  If not, see <http://www.gnu.org/licenses/>.
 -}
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP, TemplateHaskell #-}
 {-|
 Module      : Monky.Template
 Description : This module provides a template haskell splice for including librarys
@@ -44,9 +44,9 @@ this library (data <LibAlsa> and get<LibAlsa>).
 To call your functions use the record syntax on that handle.
 -}
 module Monky.Template
-( importLib
-, module Foreign.Ptr
-, module System.Posix.DynamicLinker)
+  ( importLib
+  , module Foreign.Ptr
+  )
 where
 
 import Control.Monad (liftM2)
@@ -55,6 +55,7 @@ import Data.List (nub)
 import Foreign.Ptr (Ptr, FunPtr, castFunPtr)
 import Language.Haskell.TH
 import Monky.Utility
+import Data.Maybe (fromMaybe)
 import System.Posix.DynamicLinker (DL, dlclose, dlopen, dlsym, RTLDFlags(RTLD_LAZY))
 
 #if MIN_VERSION_base(4,8,0)
@@ -64,10 +65,7 @@ import Control.Applicative ((<$>))
 
 -- trim a string
 ltrim :: String -> String
-ltrim [] = []
-ltrim (x:xs)
-  | isSpace x = ltrim xs
-  | otherwise = x:xs
+ltrim = dropWhile isSpace
 
 rtrim :: String -> String
 rtrim = reverse . ltrim . reverse
@@ -75,24 +73,16 @@ rtrim = reverse . ltrim . reverse
 trim :: String -> String
 trim = rtrim . ltrim
 
--- Split function into types
+-- Split type string
 prepareFun :: String -> [String]
 prepareFun = map trim . splitAtEvery "->" . trim
 
--- Get the type name or tell user what failed
-getJust :: String -> Maybe Name -> Q Name
-getJust _ (Just n) = return n
-getJust name Nothing = fail ("Could not find: " ++ name)
-
--- get variable name
-getVName :: String -> Q Name
-getVName name = getJust name =<< lookupValueName name
-
 -- get constructor name
 getName :: String -> Q Name
-getName name = getJust name =<< lookupTypeName name
+getName name =
+  fromMaybe (error $ "Could not find constructor: " ++ name) <$> lookupTypeName name
 
--- Get a type from a String, this also makes sure 'IO' a works
+-- Get a type from a String, this can do ONE application, so IO works
 getType :: String -> Q Type
 getType xs = if ' ' `elem` xs
   then let [t,a] = words xs in
@@ -110,54 +100,44 @@ applyArrows (x:xs) = AppT (AppT ArrowT x) (applyArrows xs)
 
 
 -- Create function declarations for the constructor
-mkFunDesc :: (String, String) -> VarStrictTypeQ
+mkFunDesc :: (String, String) -> VarBangTypeQ
 mkFunDesc (x,y) = do
   t <- applyArrows <$> mapM getType (prepareFun y)
-  return (mkName x, NotStrict, t)
+  return (mkName x, Bang NoSourceUnpackedness NoSourceStrictness, t)
 
 
-clean :: String -> String
-clean ('(':xs) = "vo" ++ clean xs
-clean (')':xs) = "id" ++ clean xs
-clean (x:xs) = x:clean xs
-clean [] = []
+cleanName :: Char -> String
+cleanName '(' = "vo"
+cleanName ')' = "id"
+cleanName  x  = return x
 
 
 -- Get the transformer name, this is some ugly name mangeling
-transName :: String -> Name
-transName = mkName . clean . ("mkFun" ++) . filter isOk
+transName :: String -> String
+transName = concatMap cleanName . ("mkFun" ++) . filter isOk
   where isOk c = not (isSpace c) && c /= '-' && c /= '>'
 
+
 -- Get the function described by the three-tuple (Alias, C-Name, TypeString)
-getFun :: Exp -> (String, String, String) -> Q Stmt
-getFun handle (alias, name, typeString) = do
-  let transname = transName typeString
-  dlsymN <- getVName "dlsym"
-  fm <- getVName "fmap"
-  dot <- getVName "."
-  cast <- getVName "castFunPtr"
-  let castFPtr = InfixE (Just (VarE transname)) (VarE dot) (Just (VarE cast))
-  let getSym = AppE (AppE (VarE dlsymN) handle) (LitE (StringL name))
-  let getF = AppE (AppE (VarE fm) castFPtr) getSym
-  return $ BindS (VarP (mkName (alias ++ "_"))) getF
+getFunQ :: Name -> (String, String, String) -> Q Stmt
+getFunQ handle (alias, name, typeString) = do
+  let castFPtr = [| $(varE . mkName $ transName typeString) . castFunPtr |]
+  let getSym = [| dlsym $(varE handle) name |]
+  BindS (VarP (mkName (alias ++ "_"))) <$> [| fmap $(castFPtr) $(getSym) |]
 
 
 -- Create the return statement, this applies the constructor
 mkRet :: Name -> [String] -> Name -> Exp -> Q Stmt
 mkRet hname xs rawN raw= do
-  let funs = map (\x -> (mkName x, VarE (mkName (x ++ "_")))) xs
-  ret <- getVName "return"
-  let con = RecConE hname ((rawN,raw):funs)
-  return $ NoBindS (AppE (VarE ret) con)
+  let funs = map (\x -> return (mkName x, VarE (mkName (x ++ "_")))) xs
+  let con = recConE hname (return (rawN,raw):funs)
+  NoBindS <$> [| return $(con) |]
 
 
 -- Create the statement to get the handle
 mkGetHandle :: Name -> String -> Q Stmt
-mkGetHandle h libname = do
-  dlopenN <- getVName "dlopen"
-  lazy <- getVName "RTLD_LAZY"
-  let hExp = AppE (AppE (VarE dlopenN) (LitE (StringL libname))) (ListE [ConE lazy])
-  return $ BindS (VarP h) hExp
+mkGetHandle h libname =
+  BindS (VarP h) <$> [| dlopen libname [RTLD_LAZY] |]
 
 
 -- Create the get<LibName> function
@@ -166,39 +146,31 @@ mkGetFun lname name hname funs raw = do
   let funName = mkName ("get" ++ name)
   let handle = mkName "handle"
   ghandle <- mkGetHandle handle lname
-  funStmts <- mapM (getFun (VarE handle)) funs
+  funStmts <- mapM (getFunQ handle) funs
   ret <- mkRet hname (map (\(x,_,_) -> x) funs) raw (VarE handle)
   let fun = FunD funName [Clause [] (NormalB $ DoE (ghandle:funStmts ++ [ret])) []]
-  io <- getName "IO"
-  let libT = mkName name
-  let sig = SigD funName (AppT (ConT io) (ConT libT))
+  sig <- sigD funName [t| IO $(conT . mkName $ name) |]
   return [sig,fun]
 
 
 -- Create the transformer function used by get<LibName>
 mkTransformer :: String -> Q Dec
 mkTransformer f = do
-  let name = transName f
-  t <- applyArrows <$> mapM getType (prepareFun f)
-  fptr <- getName "FunPtr"
-  let ftype = AppT (AppT ArrowT (AppT (ConT fptr) t)) t
-  return $ ForeignD $ ImportF CCall Safe "dynamic" name ftype
+  let name = mkName . transName $ f
+  let ty = applyArrows <$> mapM getType (prepareFun f)
+  ForeignD . ImportF CCall Safe "dynamic" name <$>  [t| (FunPtr $(ty)) -> $(ty) |]
 
 
 mkDestroyFun :: String -> Name -> Q [Dec]
 mkDestroyFun name raw = do
-  destroy <- getVName "dlclose"
-  io <- getName "IO"
   let libT = mkName name
   let hname = mkName "handle"
   let funName = mkName ("destroy" ++ name)
-  let body = AppE (VarE destroy) (AppE (VarE raw) (VarE hname))
+  let body = [| dlclose ($(varE raw) $(varE hname)) |]
 
-  let funType = AppT (AppT ArrowT (ConT libT)) (AppT (ConT io) (TupleT 0))
-  let sig = SigD funName funType
-
-  return [sig ,FunD funName [Clause [VarP hname] (NormalB body) []]]
-
+  sig <- sigD funName [t| $(conT libT) -> IO () |]
+  fun <- funD funName [clause [varP hname] (normalB body) []]
+  return [sig ,fun]
 
 -- |Import a library
 importLib
@@ -210,10 +182,9 @@ importLib hname lname xs = do
   let name = mkName hname
   funs <- mapM (mkFunDesc . (\(x,_,y) -> (x,y))) xs
   transformers <- mapM mkTransformer $ nub $ map (\(_,_,x) -> x) xs
-  rawN <- getName "DL"
   let rawRN = mkName "rawDL"
-  let raw = (rawRN, NotStrict, ConT rawN)
-  let dhandle = DataD [] name [] [RecC (mkName hname) (raw:funs)] []
+  let raw = (rawRN, Bang NoSourceUnpackedness NoSourceStrictness, ConT ''DL)
+  let dhandle = DataD [] name [] Nothing [RecC (mkName hname) (raw:funs)] []
   fun <- mkGetFun lname hname name xs rawRN
   dest <- mkDestroyFun hname rawRN
   return (dhandle:dest ++ transformers ++ fun)
