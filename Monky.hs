2969668f2419016b900efe132de3109a7544cbe9
Markus Ongyerth <ongy@ongy.net>
2017-02-15 07:21:33 +0100

    Add tick time argument and new reaction to events
    Add a new startLoopT that takes the tick time in microseconds as
    argument.
    Also make event based updates trigger a new line-write without
    advancing the tick counter.

--- Monky.hs
+++ Monky.hs
@@ -34,11 +34,13 @@ other functions later on.
 -}
 module Monky
   ( startLoop
+  , startLoopT
   )
 where
 
-
-import Control.Concurrent (threadDelay)
+import System.Timeout
+import Data.Time.Clock.POSIX
+import Control.Concurrent.MVar
 import Data.IORef (IORef, readIORef, writeIORef, newIORef, atomicWriteIORef)
 import Monky.Modules
 import Control.Monad (when)
@@ -48,15 +50,17 @@ import Control.Concurrent (forkIO)
 data ModuleWrapper = MWrapper Modules (IORef [MonkyOut])
 
 -- |Packs a module into a wrapper with an IORef for cached output
-packMod :: Modules -> IO ModuleWrapper
-packMod x@(Poll (NMW m _)) = do
-  sref <- newIORef []
-  initialize m
-  return (MWrapper x sref)
-packMod x@(Evt (DW m)) = do
-  sref <- newIORef []
-  _ <- forkIO (startEvtLoop m (atomicWriteIORef sref))
-  return $ MWrapper x sref
+packMod :: MVar Bool -> Modules -> IO ModuleWrapper
+packMod _ x@(Poll (NMW m _)) = do
+    sref <- newIORef []
+    initialize m
+    return (MWrapper x sref)
+packMod mvar x@(Evt (DW m)) = do
+    sref <- newIORef []
+    _ <- forkIO . startEvtLoop m $ \val -> do
+        atomicWriteIORef sref val
+        putMVar mvar True
+    return $ MWrapper x sref
 
 getWrapperText :: Int -> ModuleWrapper -> IO [MonkyOut]
 getWrapperText tick (MWrapper (Poll (NMW m i)) r) = do
@@ -68,16 +72,51 @@ doMonkyLine :: MonkyOutput o => Int -> o -> [ModuleWrapper] -> IO ()
 doMonkyLine t o xs =
   doLine o =<< mapM (getWrapperText t) xs
 
-mainLoop :: MonkyOutput o => Int -> o -> [ModuleWrapper] -> IO ()
-mainLoop t o xs = do
-  doMonkyLine t o xs
-  threadDelay 1000000
-  mainLoop (t+1) o xs
+doCachedLine :: MonkyOutput o => o -> [ModuleWrapper] -> IO ()
+doCachedLine out xs =
+    doLine out =<< mapM (\(MWrapper _ r) -> readIORef r) xs
 
--- |Start the mainLoop of monky. This should be the return type of your main
+waitTick :: MonkyOutput o => Int -> MVar Bool -> o -> [ModuleWrapper] -> IO ()
+waitTick limit mvar out xs = do
+    pre <- getPOSIXTime
+    ret <- timeout limit $ takeMVar mvar
+    case ret of
+        Nothing -> return ()
+        Just _ -> do
+            doCachedLine out xs
+            post <- getPOSIXTime
+            let passed = round . (* 1000000) $ post - pre
+            if passed > limit
+                then return ()
+                else waitTick (limit - passed) mvar out xs
+
+mainLoop :: MonkyOutput o => Int -> MVar Bool -> Int -> o -> [ModuleWrapper] -> IO ()
+mainLoop l r t o xs = do
+  doMonkyLine t o xs
+  waitTick l r o xs
+  mainLoop l r (t+1) o xs
+
+{- | Start the mainLoop of monky. With custom tick timer (in μs)
+
+For 1s tick-timer pass @1000000@ to this.
+This does not scale the timers for collection modules passed into it.
+So if @500000@ is passed here, and a collector should update every second
+it needs to update every 2nd tick, so it has to be scaled.
+
+@
+    startLoop getAsciiOut [ pollPack 1 $ getRawCPU ]
+
+    startLoopT 500000 getAsciiOut [ pollPack 2 $ getRawCPU ]
+@
+-}
+startLoopT :: MonkyOutput o => Int -> IO o -> [IO Modules] -> IO ()
+startLoopT t out mods = do
+    mvar <- newEmptyMVar
+    m <- sequence mods
+    l <- mapM (packMod mvar) m
+    o <- out
+    mainLoop t mvar 0 o l
+
+-- | Start the mainLoop of monky. This should be the return type of your main
 startLoop :: MonkyOutput o => IO o -> [IO Modules] -> IO ()
-startLoop o mods = do
-  m <- sequence mods
-  l <- mapM packMod m
-  out <- o
-  mainLoop 0 out l
+startLoop out mods = startLoopT 1000000 out mods
